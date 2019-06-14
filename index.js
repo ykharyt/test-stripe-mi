acc6b990d8d8147bfaf8a1f862cb23d9b51dee7f
ykharyt <ykharyt@gmail.com>
2019-06-14 14:25:35 +0300

    updated index.js

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 const express = require('express')
 const stripe = require('stripe')('sk_test_gGJy8Kzf6nIVcQKGltyvWUV400bZgJaYil');
 const bodyParser = require('body-parser')
+var app = express();
 
-const path = require('path')
-const PORT = process.env.PORT || 5000
+app.set('port', (process.env.PORT || 5000));
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all temaplate files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.post('/ephemeral_keys', (req, res) => { 
+	var customerId = req.body.customer_id;
+	var api_version = req.body.api_version;
+
+	stripe.ephemeral.create(
+		{customer : customerId},
+		{stripe_version : api_version}
+	).then((key) => {
+		res.status(200).send(key)
+	}).catch((err) => {
+		res.status(500).end()
+	});
+});
+
+app.listen(app.get('port'), function() {
+	console.log('Node app is running on port', app.get('port'));
+});
